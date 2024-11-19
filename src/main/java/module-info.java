cc9f5f961d1a4e4e08187d566ecda36592ef5e8a
Udynix <129510560+LinaTopakova@users.noreply.github.com>
2024-11-20 05:09:02 +0700

    commit4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example.javafx to javafx.fxml;
--- src/main/java/org/example/javafx/Exercise3.java
+++ src/main/java/org/example/javafx/Exercise3.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -51,37 +52,51 @@ public class Exercise3 extends Application {
         menu.add(product);
         cappuccino.setMinWidth(300);
 
-        Button latte = new Button("Латте            -     100р");
+        TextField latte = new TextField("Латте            -     100р");
+        latte.setEditable(false);
+        latte.setAlignment(Pos.CENTER);
         product = new Menu("Латте",110);
         menu.add(product);
         latte.setMinWidth(300);
 
-        Button cocaCola = new Button("Кола              -     100р");
+        TextField cocaCola = new TextField("Кола              -     100р");
+        cocaCola.setEditable(false);
+        cocaCola.setAlignment(Pos.CENTER);
         product = new Menu("Кола",90);
         menu.add(product);
         cocaCola.setMinWidth(300);
 
-        Button cheeseburger = new Button("Чизбургер     -     100р");
+        TextField cheeseburger = new TextField("Чизбургер     -     100р");
+        cheeseburger.setEditable(false);
+        cheeseburger.setAlignment(Pos.CENTER);
         product = new Menu("Чизбургер",150);
         menu.add(product);
         cheeseburger.setMinWidth(300);
 
-        Button grandee = new Button("Гранд            -     100р");
+        TextField grandee = new TextField("Гранд            -     100р");
+        grandee.setEditable(false);
+        grandee.setAlignment(Pos.CENTER);
         product = new Menu("Гранд",200);
         menu.add(product);
         grandee.setMinWidth(300);
 
-        Button bigHit = new Button("Биг Хит          -     100р");
+        TextField bigHit = new TextField("Биг Хит          -     100р");
+        bigHit.setEditable(false);
+        bigHit.setAlignment(Pos.CENTER);
         product = new Menu("Биг Хит",180);
         menu.add(product);
         bigHit.setMinWidth(300);
 
-        Button frenchFries = new Button("Картофель     -     100р");
+        TextField frenchFries = new TextField("Картофель     -     100р");
+        frenchFries.setEditable(false);
+        frenchFries.setAlignment(Pos.CENTER);
         product = new Menu("Картофель Фри",120);
         menu.add(product);
         frenchFries.setMinWidth(300);
 
-        Button nuggets = new Button("Наггетсы         -     100р");
+        TextField nuggets = new TextField("Наггетсы         -     100р");
+        nuggets.setEditable(false);
+        nuggets.setAlignment(Pos.CENTER);
         product = new Menu("Наггетсы",140);
         menu.add(product);
         nuggets.setMinWidth(300);
@@ -89,10 +104,31 @@ public class Exercise3 extends Application {
         TextArea textArea = new TextArea();
         textArea.setPrefWidth(350);
 
-        Spinner<Integer> quantitySpinner = new Spinner<>(1, 10, 1);
-        HBox box2 = new HBox(10,cappuccino,quantitySpinner);
+        Spinner<Integer> quantitySpinner1 = new Spinner<>(0, 69, 0);
+        HBox box2 = new HBox(10,cappuccino,quantitySpinner1);
 
-        VBox box = new VBox(0,box2 , latte, cocaCola, cheeseburger, grandee, bigHit, frenchFries, nuggets);
+        Spinner<Integer> quantitySpinner2 = new Spinner<>(0, 69, 0);
+        HBox box3 = new HBox(10,latte,quantitySpinner2);
+
+        Spinner<Integer> quantitySpinner3 = new Spinner<>(0, 69, 0);
+        HBox box4 = new HBox(10,cocaCola,quantitySpinner3);
+
+        Spinner<Integer> quantitySpinner4 = new Spinner<>(0, 69, 0);
+        HBox box5 = new HBox(10,cheeseburger,quantitySpinner4);
+
+        Spinner<Integer> quantitySpinner5 = new Spinner<>(0, 69, 0);
+        HBox box6 = new HBox(10,grandee,quantitySpinner5);
+
+        Spinner<Integer> quantitySpinner6 = new Spinner<>(0, 69, 0);
+        HBox box7 = new HBox(10,bigHit,quantitySpinner6);
+
+        Spinner<Integer> quantitySpinner7 = new Spinner<>(0, 69, 0);
+        HBox box8 = new HBox(10,frenchFries,quantitySpinner7);
+
+        Spinner<Integer> quantitySpinner8 = new Spinner<>(0, 69, 0);
+        HBox box9 = new HBox(10,nuggets,quantitySpinner8);
+
+        VBox box = new VBox(10,box2 , box3, box4, box5, box6, box7, box8, box9);
         box.setLayoutX(150);
         box.setLayoutY(20);
 
